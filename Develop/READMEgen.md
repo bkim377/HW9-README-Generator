e7bc96b2769e3d3e4a11225ba4e9ac0b7882b65e
Brandon Kim <bkim377@gatech.edu>
2020-03-26 15:54:24 -0400

    Checkpoint 5 - added badge line and tested out writeFile

--- Develop/READMEgen.md
+++ Develop/READMEgen.md
@@ -1,19 +1,18 @@
-https://avatars0.githubusercontent.com/u/61259374?v=4
-<h1>Project Name: HW9-ReadME-Generator</h1>
-      <hr></hr>
-      <h3>Project Description: </h3><p>My submission for HW9</p>
-      <h3>Table of Contents: </h3><ol>*Project Name
- *Description
- *Install Instructions
- *Usage
- *Contributors
- *Tests
- *Questions</ol>
+<h1>Project Name: HW9-GoodREADME</h1>
+      <img src="https://img.shields.io/static/v1?label=License&message=GNU&color=blue" alt="Badge for License"></img>
+        <hr>
+      <h3>Project Description: </h3><p>HW9 submission</p>
+      <h3>Table of Contents: </h3><ol>* Project Name
+  * Description
+  * Install Instructions
+  * Usage
+  * Contributors
+  * Tests
+  * Questions</ol>
       <h3>Installation Instructions: </h3><p>Run npm install in correct directory</p>
       <h3>Usage: </h3><p>follow directions above</p>
-      <h3>Contributing Users: </h3><p>Brandon Kim</p>
+      <h3>Contributing Users: </h3><p>Brandon G. Kim</p>
       <h3>Tests: </h3><p>None</p>
       <h3>Questions: </h3><p>None</p>
-      <h2>Creator's Email: </h2><p>null</p>
       <h2>Creator's Picture: </h2><img src=https://avatars0.githubusercontent.com/u/61259374?v=4 alt="User Profile Pic"></img>
       
--- Develop/index.js
+++ Develop/index.js
@@ -21,56 +21,52 @@ const questions = [
 inquirer
   .prompt([
     { type: "input", 
-    message: questions[0], 
-    name: "username" },
+      message: questions[0], 
+      name: "username" },
     { type: "list",
       message: questions[1],
       name: "license",
-      choices: ["MIT", "Mozilla", "GNU", "Apache", "Boost", "Unlicense"]
-    },
+      choices: ["MIT", "Mozilla", "GNU", "Apache", "Boost", "Unlicense"]},
     { type: "input", 
       message: questions[2], 
       name: "projectName" },
     { type: "input", 
       message: questions[3], 
       name: "projectDescription" },
-    {
-      type: "input",
+    { type: "input",
       message: questions[4],
       name: "tableOfContents",
-      default: `*Project Name\n *Description\n *Install Instructions\n *Usage\n *Contributors\n *Tests\n *Questions`
-    },
+      default: `* Project Name\n  * Description\n  * Install Instructions\n  * Usage\n  * Contributors\n  * Tests\n  * Questions` },
     { type: "input",
       message: questions[5],
       name: "installInstructions",
-      default: "Run npm install in correct directory"
-    },
+      default: "Run npm install in correct directory" },
     { type: "input",
       message: questions[6],
       name: "usage",
-      default: "follow directions above"
-    },
+      default: "follow directions above" },
     { type: "input",
       message: questions[7],
       name: "contributors",
-      default: "None"
-    },
+      default: "None" },
     { type: "input", 
-    message: questions[8], 
-    name: "tests", 
-    default: "None" },
+      message: questions[8], 
+      name: "tests", 
+      default: "None" },
     { type: "input", 
-    message: questions[9], 
-    name: "questions", 
-    default: "None" }
+      message: questions[9], 
+      name: "questions", 
+      default: "None" }
   ])
   .then(response => {
     const queryURL = `https://api.github.com/users/${response.username}`;
-    axios.get(queryURL).then(function(res) {
-      console.log(res.data);
-      const fileContent = 
-      `<h1>Project Name: ${response.projectName}</h1>
-      <hr></hr>
+    axios
+      .get(queryURL)
+      .then(function(res) {
+        // console.log(res.data);
+        const fileContent = `<h1>Project Name: ${response.projectName}</h1>
+      <img src="https://img.shields.io/static/v1?label=License&message=${response.license}&color=blue" alt="Badge for License"></img>
+        <hr>
       <h3>Project Description: </h3><p>${response.projectDescription}</p>
       <h3>Table of Contents: </h3><ol>${response.tableOfContents}</ol>
       <h3>Installation Instructions: </h3><p>${response.installInstructions}</p>
@@ -78,22 +74,20 @@ inquirer
       <h3>Contributing Users: </h3><p>${response.contributors}</p>
       <h3>Tests: </h3><p>${response.tests}</p>
       <h3>Questions: </h3><p>${response.questions}</p>
-      <h2>Creator's Email: </h2><p>${res.data.email}</p>
       <h2>Creator's Picture: </h2><img src=${res.data.avatar_url} alt="User Profile Pic"></img>
-      `
+      `;
 
-      fs.appendFile("READMEgen.md", fileContent, function(err) {
-        if (err) {
-          throw err;
-        }
-        console.log("Successfully written your README file");
+        fs.writeFile("READMEgen.md", fileContent, function(err) {
+          if (err) {
+            throw err;
+          }
+          console.log("Successfully written your README file");
+        });
+      })
+      .catch(function(err) {
+        console.log(err);
       });
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
   });
-
 
 // function writeToFile(fileName, data) {}
 
