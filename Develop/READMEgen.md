4704267828c3ffbb2e534448559bf5e661278922
Brandon Kim <bkim377@gatech.edu>
2020-03-26 08:54:03 -0400

    Checkpoint 4 - added all questions and made the filewrite work

--- Develop/READMEgen.md
+++ Develop/READMEgen.md
@@ -1,1 +1,19 @@
 https://avatars0.githubusercontent.com/u/61259374?v=4
+<h1>Project Name: HW9-ReadME-Generator</h1>
+      <hr></hr>
+      <h3>Project Description: </h3><p>My submission for HW9</p>
+      <h3>Table of Contents: </h3><ol>*Project Name
+ *Description
+ *Install Instructions
+ *Usage
+ *Contributors
+ *Tests
+ *Questions</ol>
+      <h3>Installation Instructions: </h3><p>Run npm install in correct directory</p>
+      <h3>Usage: </h3><p>follow directions above</p>
+      <h3>Contributing Users: </h3><p>Brandon Kim</p>
+      <h3>Tests: </h3><p>None</p>
+      <h3>Questions: </h3><p>None</p>
+      <h2>Creator's Email: </h2><p>null</p>
+      <h2>Creator's Picture: </h2><img src=https://avatars0.githubusercontent.com/u/61259374?v=4 alt="User Profile Pic"></img>
+      
--- Develop/index.js
+++ Develop/index.js
@@ -20,135 +20,80 @@ const questions = [
 // the inquirer prompt that asks the questions listed above
 inquirer
   .prompt([
-    { type: "input", message: questions[0], name: "username" },
-    {
-      type: "list",
+    { type: "input", 
+    message: questions[0], 
+    name: "username" },
+    { type: "list",
       message: questions[1],
       name: "license",
       choices: ["MIT", "Mozilla", "GNU", "Apache", "Boost", "Unlicense"]
     },
-    { type: "input", message: questions[2], name: "projectName" },
-    { type: "input", message: questions[3], name: "projectDescription" },
+    { type: "input", 
+      message: questions[2], 
+      name: "projectName" },
+    { type: "input", 
+      message: questions[3], 
+      name: "projectDescription" },
     {
       type: "input",
       message: questions[4],
       name: "tableOfContents",
       default: `*Project Name\n *Description\n *Install Instructions\n *Usage\n *Contributors\n *Tests\n *Questions`
     },
-    {
-      type: "input",
+    { type: "input",
       message: questions[5],
       name: "installInstructions",
       default: "Run npm install in correct directory"
     },
-    {
-      type: "input",
+    { type: "input",
       message: questions[6],
       name: "usage",
       default: "follow directions above"
     },
-    {
-      type: "input",
+    { type: "input",
       message: questions[7],
       name: "contributors",
       default: "None"
     },
-    { type: "input", message: questions[8], name: "tests", default: "None" },
-    { type: "input", message: questions[9], name: "questions", default: "None" }
+    { type: "input", 
+    message: questions[8], 
+    name: "tests", 
+    default: "None" },
+    { type: "input", 
+    message: questions[9], 
+    name: "questions", 
+    default: "None" }
   ])
   .then(response => {
     const queryURL = `https://api.github.com/users/${response.username}`;
     axios.get(queryURL).then(function(res) {
       console.log(res.data);
-      const {avatar_url} = res.data;
-      // const profilePic = window.print(avatar_url);
+      const fileContent = 
+      `<h1>Project Name: ${response.projectName}</h1>
+      <hr></hr>
+      <h3>Project Description: </h3><p>${response.projectDescription}</p>
+      <h3>Table of Contents: </h3><ol>${response.tableOfContents}</ol>
+      <h3>Installation Instructions: </h3><p>${response.installInstructions}</p>
+      <h3>Usage: </h3><p>${response.usage}</p>
+      <h3>Contributing Users: </h3><p>${response.contributors}</p>
+      <h3>Tests: </h3><p>${response.tests}</p>
+      <h3>Questions: </h3><p>${response.questions}</p>
+      <h2>Creator's Email: </h2><p>${res.data.email}</p>
+      <h2>Creator's Picture: </h2><img src=${res.data.avatar_url} alt="User Profile Pic"></img>
+      `
 
-      fs.writeFile("READMEgen.md", avatar_url + "\n", function(err) {
+      fs.appendFile("READMEgen.md", fileContent, function(err) {
         if (err) {
           throw err;
         }
-        console.log(avatar_url);
+        console.log("Successfully written your README file");
       });
     })
     .catch(function(err) {
       console.log(err);
     });
   });
-// },
-//
-// })
-// .then(function({ username }) {
-//   const queryUrl = `https://api.github.com/users/${username}`;
 
-//   axios.get(queryUrl).then(function(res) {
-      
-//       fs.appendFile("READMEgen.md", email, function(err) {
-//         if (err) {
-//           throw err;
-//         }
-//         console.log(email);
-//       });
-//     })
-//     .catch(function(err) {
-//       console.log(err);
-//     });
-//     });
-
-// message: questions[1],
-// name: "badgeLabel"
-//   ([
-
-//   {
-//     type: "input",
-//     message: questions[2],
-//     name: "badgeContent"
-//   },
-//   {
-//     type: "input",
-//     message: questions[3],
-//     name: "projectName"
-//   },
-//   {
-//     type: "input",
-//     message: questions[4],
-//     name: "projectDescription"
-//   },
-//   {
-//     type: "input",
-//     message: questions[5],
-//     name: "tableOfContents"
-//   },
-//   {
-//     type: "input",
-//     message: questions[6],
-//     name: "installation"
-//   },
-//   {
-//     type: "input",
-//     message: questions[7],
-//     name: "usage"
-//   },
-//   {
-//     type: "input",
-//     message: questions[8],
-//     name: "license"
-//   },
-//   {
-//     type: "input",
-//     message: questions[9],
-//     name: "contributors"
-//   },
-//   {
-//     type: "input",
-//     message: questions[10],
-//     name: "tests"
-//   },
-//   {
-//     type: "input",
-//     message: questions[11],
-//     name: "questions"
-//   },
-// ]);
 
 // function writeToFile(fileName, data) {}
 
